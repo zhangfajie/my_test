a107cefacd9897820ff615c924c57dcaf6924a13
zhangfajie <1550380054@qq.com>
2020-07-29 16:04:15 +0800

    2020 的test1提交了

---
+++ src/main/java/com/itcast/Controller/test1Controller.java
@@ -0,0 +1,38 @@
+package com.itcast.Controller;
+
+
+import com.itcast.config.WebSocketServer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+
+/**
+ * WebSocketController
+ * @author zhengkai.blog.csdn.net
+ */
+@RestController
+@RequestMapping("test/")
+public class test1Controller {
+
+    @GetMapping("index")
+    public ResponseEntity<String> index(){
+        return ResponseEntity.ok("请求成功");
+    }
+
+    @GetMapping("page")
+    public ModelAndView page(){
+        return new ModelAndView("websocket");
+    }
+
+    @RequestMapping("/push/{toUserId}")
+    public ResponseEntity<String> pushToWeb(String message, @PathVariable String toUserId) throws IOException {
+        WebSocketServer web=new WebSocketServer();
+        web.sendInfo(message,toUserId);
+        return ResponseEntity.ok("MSG SEND SUCCESS");
+    }
+}
