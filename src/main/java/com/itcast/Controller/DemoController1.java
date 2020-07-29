53ddea119fc5aad7c8a7831e718c5c45ca743e3a
zhangfajie <1550380054@qq.com>
2020-07-29 16:08:18 +0800

    2020 的test1提交了

--- src/main/java/com/itcast/Controller/DemoController1.java
+++ src/main/java/com/itcast/Controller/DemoController1.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("page")
 public class DemoController1 {
 
-    //分支1  编号2
+    //测试  2020  第一次提交  编号为2
     @GetMapping("index")
     public String page(){
 
